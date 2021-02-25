f1b462ed23752a0007646ec044892f13708fc65c
Dan Hayworth <dan.hayworth@hotmail.com>
2021-02-25 21:35:50 +1300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace NewProject2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.txtEditor = new System.Windows.Forms.TextBox();
+            this.opnTextFile = new System.Windows.Forms.OpenFileDialog();
+            this.savTextFile = new System.Windows.Forms.SaveFileDialog();
+            this.mnuMain = new System.Windows.Forms.MenuStrip();
+            this.mnuMainToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuWordWrap = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // txtEditor
+            // 
+            this.txtEditor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtEditor.Location = new System.Drawing.Point(0, 31);
+            this.txtEditor.Multiline = true;
+            this.txtEditor.Name = "txtEditor";
+            this.txtEditor.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtEditor.Size = new System.Drawing.Size(776, 669);
+            this.txtEditor.TabIndex = 0;
+            this.txtEditor.WordWrap = false;
+            // 
+            // mnuMain
+            // 
+            this.mnuMain.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.mnuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuMainToolStripMenuItem,
+            this.formatToolStripMenuItem});
+            this.mnuMain.Location = new System.Drawing.Point(0, 0);
+            this.mnuMain.Name = "mnuMain";
+            this.mnuMain.Size = new System.Drawing.Size(776, 28);
+            this.mnuMain.TabIndex = 3;
+            this.mnuMain.Text = "menuStrip1";
+            // 
+            // mnuMainToolStripMenuItem
+            // 
+            this.mnuMainToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.openToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.saveAsToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.mnuMainToolStripMenuItem.Name = "mnuMainToolStripMenuItem";
+            this.mnuMainToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.mnuMainToolStripMenuItem.Text = "&File";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(224, 26);
+            this.toolStripMenuItem2.Text = "&New";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.openToolStripMenuItem.Text = "&Open...";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(224, 26);
+            this.toolStripMenuItem1.Text = "&Save";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.saveAsToolStripMenuItem.Text = "&Save As...";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(221, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // formatToolStripMenuItem
+            // 
+            this.formatToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuWordWrap});
+            this.formatToolStripMenuItem.Name = "formatToolStripMenuItem";
+            this.formatToolStripMenuItem.Size = new System.Drawing.Size(70, 24);
+            this.formatToolStripMenuItem.Text = "Format";
+            // 
+            // mnuWordWrap
+            // 
+            this.mnuWordWrap.Name = "mnuWordWrap";
+            this.mnuWordWrap.Size = new System.Drawing.Size(224, 26);
+            this.mnuWordWrap.Text = "Word Wrap";
+            this.mnuWordWrap.Click += new System.EventHandler(this.mnuWordWrap_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(776, 701);
+            this.Controls.Add(this.txtEditor);
+            this.Controls.Add(this.mnuMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.mnuMain;
+            this.Name = "Form1";
+            this.Text = "Text editor";
+            this.mnuMain.ResumeLayout(false);
+            this.mnuMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.TextBox txtEditor;
+        private System.Windows.Forms.OpenFileDialog opnTextFile;
+        private System.Windows.Forms.SaveFileDialog savTextFile;
+        private System.Windows.Forms.MenuStrip mnuMain;
+        private System.Windows.Forms.ToolStripMenuItem mnuMainToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuWordWrap;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.IO;
+
+namespace NewProject2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string lastFileName = "";
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TextReader dan;
+            opnTextFile.Filter = "Text Files(*.txt)|*.txt|" + "All Files(*.*)|*.*";
+            opnTextFile.ShowDialog();
+            lastFileName = opnTextFile.FileName;
+            if (opnTextFile.FileName != "")
+            {
+                try
+                {
+                    dan = File.OpenText(opnTextFile.FileName);
+                    txtEditor.Text = dan.ReadToEnd();
+                    dan.Close();
+                }
+
+                catch (Exception nica)
+                {
+                    MessageBox.Show("Nada");
+                }
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveAs();
+        }
+        private void SaveAs()
+        {
+            SaveFileDialog savText = new SaveFileDialog();
+            savText.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            savText.ShowDialog();
+            lastFileName = savText.FileName;
+            if (savText.FileName != "")
+            {
+                using (StreamWriter outputFile = new StreamWriter(savText.OpenFile()))
+                {
+                    foreach (string line in txtEditor.Lines)
+                        outputFile.WriteLine(line);
+                }
+            }
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (lastFileName != "")
+            {
+                using (StreamWriter outputFile = new StreamWriter(lastFileName))
+                {
+                    foreach (string line in txtEditor.Lines)
+                        outputFile.WriteLine(line);
+                }
+            }
+            else
+            {
+                SaveAs();
+            }
+
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            txtEditor.Text = "";
+            lastFileName = "";
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void mnuWordWrap_Click(object sender, EventArgs e)
+        {
+            mnuWordWrap.Checked = !mnuWordWrap.Checked;
+            txtEditor.Select(0, 0);
+        }
+    }
+
+    
+}
